4767000f6e150c883435fdaa0c22681ce72d9f7d
luca3 <dudsj9782@gmail.com>
2024-08-23 08:23:31 +0900

    레전드 가르기

--- ParkingCar/Assets/01.Scripts/YS/CarPart.cs
+++ ParkingCar/Assets/01.Scripts/YS/CarPart.cs
@@ -1,33 +1,45 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CarPart : MonoBehaviour
 {
     private FixedJoint _fixedJoint;
     [HideInInspector] public Rigidbody rigid;
+    public List<CarPart> group;
     
-    private CarWheel _wheel;
+    private CarWheel wheel;
 
-    public Vector3 Dir => _wheel.transform.forward;
+    public Vector3 Dir => wheel.transform.forward;
 
     public void SetUp()
     {
         rigid = GetComponent<Rigidbody>();
-        _wheel = GetComponentInChildren<CarWheel>();
-        _wheel.SetUp();
+        wheel = GetComponentInChildren<CarWheel>();
+        wheel.SetUp();
+        
+        group = new();
+        group.Add(this);
     }
 
-    public void Connect(Rigidbody other)
+    public void Connect(CarPart otherPart)
     {
+        group.Add(otherPart);
         _fixedJoint = gameObject.AddComponent<FixedJoint>();
-        _fixedJoint.connectedBody = other;
+        _fixedJoint.connectedBody = otherPart.rigid;
     }
 
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Wall"))
         {
-            
+            Vector3 reflect = Vector3.Reflect(transform.forward, other.contacts[0].normal);
+            foreach (var carPart in group)
+            {
+                carPart.rigid.velocity = Vector3.zero;
+                carPart.transform.forward = reflect;
+                carPart.wheel.transform.localRotation = Quaternion.identity;
+            }
         }
     }
 }
