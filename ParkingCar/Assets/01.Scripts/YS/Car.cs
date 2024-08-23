246da4c05f5f1ee126e4e7f9905b5f93eca44f4d
luca3 <dudsj9782@gmail.com>
2024-08-23 23:36:31 +0900

    스테이지 수정

--- ParkingCar/Assets/01.Scripts/YS/Car.cs
+++ ParkingCar/Assets/01.Scripts/YS/Car.cs
@@ -21,8 +21,6 @@ public class Car : MonoBehaviour
 
     private bool _topLeft, _topRight, _bottomLeft, _bottomRight;
 
-    public int _successPartCount = 4;
-
     private void Awake()
     {
         _top.left.SetUp();
